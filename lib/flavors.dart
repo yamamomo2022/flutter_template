9dce99c76ce402e244b4b1b70a6ba8a37bff11b7
yamamomo <violetraisondetre@gmail.com>
2025-08-29 20:45:43 +0900

    feat:set flavor

---
+++ lib/flavors.dart
@@ -0,0 +1,16 @@
+enum Flavor { dev, prod }
+
+class F {
+  static late final Flavor appFlavor;
+
+  static String get name => appFlavor.name;
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.dev:
+        return 'Flavor Template App Dev';
+      case Flavor.prod:
+        return 'Flavor Template App';
+    }
+  }
+}
