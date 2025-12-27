88ae77b73d6b31061d03575bc3e82c7e5f984f74
yamamomo <violetraisondetre@gmail.com>
2025-12-27 23:09:10 +0900

    fix: remove _flavorBanner

--- lib/app.dart
+++ lib/app.dart
@@ -37,23 +37,6 @@ class App extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF303030),
         textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.white)),
       ),
-      // home: _flavorBanner(child: CatalogPage(), show: kDebugMode), // router使用時は不要
     );
   }
-
-  Widget _flavorBanner({required Widget child, bool show = true}) =>
-      show
-          ? Banner(
-            location: BannerLocation.topStart,
-            message: F.name,
-            color: Colors.green.withAlpha(150),
-            textStyle: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 12.0,
-              letterSpacing: 1.0,
-            ),
-            textDirection: TextDirection.ltr,
-            child: child,
-          )
-          : Container(child: child);
 }
