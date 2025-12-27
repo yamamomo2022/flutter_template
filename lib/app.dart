ef1cbc7d57cb2fdfa5c6d210209c6821f66e35fc
yamamomo <violetraisondetre@gmail.com>
2025-12-27 23:02:51 +0900

    fix: copilot review

--- lib/app.dart
+++ lib/app.dart
@@ -14,7 +14,6 @@ class App extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
-      title: 'Catalog',
       onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
       localizationsDelegates: [
         AppLocalizations.delegate,
